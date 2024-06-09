d134ec3c1d984626df6e603ce3977db102c50c0a
PranovAdilet <pranov.adiletqwe@mail.ru>
2024-06-10 00:15:39 +0600

    fixed bug with GP deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
     alias: {
